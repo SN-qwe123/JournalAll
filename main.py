9d331a5f3015b11f19491c98dd1d4b3a661f22eb
justinn <buptnsy@gmail.com>
2025-04-19 21:50:48 -0400

    menu created

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+menu = """Please select:
+1, Add an Entry
+2, View All Entries
+3, Exit
+
+Your Selection is:
+"""
+user_input = input(menu)
+
+while user_input != 3:
+    if user_input == "1":
+        print("1")
+    elif user_input == "2":
+        print("2")
+    else:
+        print("invalid input.")
