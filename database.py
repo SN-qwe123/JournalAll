4d80213161797dc3358d64a47f5397c4b599bcd2
justinn <buptnsy@gmail.com>
2025-04-20 10:03:42 -0400

    init functions to create and read entries

---
+++ database.py
@@ -0,0 +1,10 @@
+entries = []
+
+def enter_entry():
+    entry_content = input("What have you learnt today?")
+    entry_date = input("What date?")
+    entries.append({"content": entry_content, "date": entry_date})
+
+def view_entries():
+    for entry in entries:
+        print(f"{entry['date']}\n{entry['content']}\n\n")
